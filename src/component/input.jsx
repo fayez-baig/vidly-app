226c43304cd1fb0e6b1d95ce1e540f9e2fee5f84
fayez baig <coderninja16@gmail.com>
2020-07-25 23:20:34 +0530

    using rest operator in input to accept props

--- src/component/input.jsx
+++ src/component/input.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, onChange, value, error, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        name={name}
-        onChange={onChange}
-        value={value}
-        type={type}
-        className="form-control"
-      />
+      <input name={name} {...rest} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
