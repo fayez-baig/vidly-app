192df49eaa9f693af38cd3ca1205a665be83cfed
fayez baig <coderninja16@gmail.com>
2020-07-27 22:58:57 +0530

    refactoring

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,24 +1,24 @@
 import http from "./httpService";
-
+import config from "./config.json";
 const getMovies = () => {
-  return http.get("http://localhost:3900/api/movies");
+  return http.get(`${config.apiEndPoint}/movies/`);
 };
 const getMovie = (id) => {
-  return http.get(`http://localhost:3900/api/movies/${id}`);
+  return http.get(`${config.apiEndPoint}/movies/${id}`);
 };
 
 const deleteMovie = (id) => {
-  return http.delete(`http://localhost:3900/api/movies/${id}`);
+  return http.delete(`${config.apiEndPoint}/movies/${id}`);
 };
 
 const saveMovie = (movie) => {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(`http://localhost:3900/api/movies/${movie._id}`, body);
+    return http.put(`${config.apiEndPoint}/movies/${movie._id}`, body);
   }
 
-  return http.post(`http://localhost:3900/api/movies`, movie);
+  return http.post(`${config.apiEndPoint}/movies`, movie);
 };
 
 export { getMovies, getMovie, deleteMovie, saveMovie };
